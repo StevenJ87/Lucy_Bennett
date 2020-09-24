2f8e886bc7020cd16c9988f33fdcb99873b11c72
Steven Johnson <sjohnson21960@gmail.com>
2020-09-24 15:25:38 -0500

    portfolio images

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -2,21 +2,21 @@ import React, { Component } from "react";
 import { Row, Container } from "../components/Grid";
 import "./index.css"
 import { Carousel } from "react-bootstrap"
-import longcut1 from "../images/longcut1.PNG"
-import longcut2 from "../images/longcut2.PNG"
-import longcut3 from "../images/longcut3.PNG"
-import shortcut1 from "../images/shortcut1.PNG"
-import shortcut2 from "../images/shortcut2.PNG"
-import shortcut3 from "../images/shortcut3.PNG"
-import natcolor1 from "../images/natcolor1.PNG"
-import natcolor2 from "../images/natcolor2.PNG"
-import natcolor3 from "../images/natcolor3.PNG"
-import fashcolor1 from "../images/fashcolor1.PNG"
-import fashcolor2 from "../images/fashcolor2.PNG"
-import fashcolor3 from "../images/fashcolor3.PNG"
-import style1 from "../images/style1.PNG"
-import style2 from "../images/style2.PNG"
-import style3 from "../images/style3.PNG"
+import longcut1 from "../images/longcut1.jpg"
+import longcut2 from "../images/longcut2.jpg"
+import longcut3 from "../images/longcut3.jpg"
+import shortcut1 from "../images/shortcut1.jpg"
+import shortcut2 from "../images/shortcut2.jpg"
+import shortcut3 from "../images/shortcut3.jpg"
+import natcolor1 from "../images/natcolor1.jpg"
+import natcolor2 from "../images/natcolor2.jpg"
+import natcolor3 from "../images/natcolor3.jpg"
+import fashcolor1 from "../images/fashcolor1.jpg"
+import fashcolor2 from "../images/fashcolor2.jpg"
+import fashcolor3 from "../images/fashcolor3.jpg"
+import style1 from "../images/style1.jpg"
+import style2 from "../images/style2.jpg"
+import style3 from "../images/style3.jpg"
 
 class Projects extends Component {
   render() {
@@ -59,21 +59,21 @@ class Projects extends Component {
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
-                    src={longcuts1}
+                    src={longcut1}
                     alt="First slide"
                   />
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
-                    src={longcuts2}
+                    src={longcut2}
                     alt="Third slide"
                   />
                 </Carousel.Item>
                 <Carousel.Item>
                   <img
                     className="d-block w-100"
-                    src={longcuts3}
+                    src={longcut3}
                     alt="Third slide"
                   />
                 </Carousel.Item>
