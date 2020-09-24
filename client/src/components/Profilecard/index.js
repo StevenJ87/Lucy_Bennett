b78eab9bde4e455f76f3632c844420c51131b8e7
Steven Johnson <sjohnson21960@gmail.com>
2020-09-24 12:04:18 -0500

    styling footer

--- client/src/components/Profilecard/index.js
+++ client/src/components/Profilecard/index.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "./style.css";
 import profilePic from "../../images/profile.jpg"
-import {ExternalLink} from "react-external-link"
+import { ExternalLink } from "react-external-link"
+import { Card, Button, ModalFooter } from 'react-bootstrap'
+import {Col, Row} from "../Grid"
+import Insta from "../../images/Instagram.png"
+import Face from "../../images/facebook.png"
 
 function ProfileCard() {
   return (
-      <div className="card" id="profile-card">
-        <img id="profile-pic" src={profilePic} className="card-img-top" alt="Profile" />
-        <div className="card-body">
-          <h5 className="card-title">Steven Johnson</h5>
-          <ExternalLink href="https://www.instagram.com/color_me_lucky/" className="btn">Instagram</ExternalLink>
-          <ExternalLink href="https://www.facebook.com/lucybennett.styles" className="btn">Facebook</ExternalLink>
+    <div className="footer">
+      <div className="container">
+        <div className="info">
+          <div className="col-md-12">
+          <ExternalLink href="https://www.instagram.com/color_me_lucky/" className="btn"><img className="icon" src={Insta}/></ExternalLink>
+          <ExternalLink href="https://www.facebook.com/lucybennett.styles" className="btn"><img className="icon" src={Face}/></ExternalLink>
+          </div>
         </div>
       </div>
+    </div>
   );
 }
 
