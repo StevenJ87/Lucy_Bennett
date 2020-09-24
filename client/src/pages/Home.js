e4647288bea21022543e267fdacaa445d233950e
Steven Johnson <sjohnson21960@gmail.com>
2020-09-24 15:30:16 -0500

    about me changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -16,10 +16,10 @@ class Home extends Component {
                 Hello there friends!
                 </p>
               <p>
-                My name is Steven Johnson and it is truly a pleasure to have you here.
+                My name is Lucy Bennett and it is truly a pleasure to have you here.
                 </p>
                 <p>
-                  I am a Full stack web developer with engineering experience educated at Massachusetts Maritime Academy. I have experience with HTML, CSS, Javascript, GitHub, Bootswatch, VS Code and a variety of coding related applications. I have a lot of strength in interpersonal relationships and communication from my history of holding management positions and working in close quarters. I have success working with teams in dangerous and stressful working conditions including deep sea rigs and isolated work stations including Antarctica. I am looking forward to working with experienced web developers on complicated projects that benefit all communities.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                   </p>
             </div>
             </div>
