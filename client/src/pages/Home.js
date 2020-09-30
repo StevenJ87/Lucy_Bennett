bf4151001d3f6e1d1c5bb4e996c8e4800c9ebf10
Steven Johnson <sjohnson21960@gmail.com>
2020-09-30 11:42:40 -0500

    Home page update

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -19,7 +19,10 @@ class Home extends Component {
                 My name is Lucy Bennett and it is truly a pleasure to have you here.
                 </p>
                 <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  I am an Artistic, creative, and patient cosmetologist with an extensive customer service background and an upbringing in the hair styling world! 
+                  </p>
+                <p>
+                Come with me on this journey through the fantastic world of hair care! On this page you will find all the cuts, colors, and styles I have performed. I hope to never stop learning new things!!
                   </p>
             </div>
             </div>
