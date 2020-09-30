0baa634754a546b926496892019f75cd27307018
Steven Johnson <sjohnson21960@gmail.com>
2020-09-30 11:26:49 -0500

    update short cuts

--- client/src/pages/ShortCuts.js
+++ client/src/pages/ShortCuts.js
@@ -16,7 +16,22 @@ let shortcuts = [
   'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120250904_10224237376350931_370928656275849717_n.jpg?_nc_cat=110&_nc_sid=0debeb&_nc_ohc=ChOWeEDofdoAX9RZmK_&_nc_ht=scontent-dfw5-1.xx&oh=6844c5a749c66cde8b4a939cd33853e6&oe=5F95227F',
   'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120260917_10224237446112675_9171576474610392466_n.jpg?_nc_cat=108&_nc_sid=0debeb&_nc_ohc=ZF6i6oVeXukAX-HYq-t&_nc_ht=scontent-dfw5-2.xx&oh=113a603b01a565a175106a4b3cbc346e&oe=5F9558FE',
   'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120292229_10224237446512685_6897692524845047866_n.jpg?_nc_cat=100&_nc_sid=0debeb&_nc_ohc=nGCnQboVLNcAX9aI8b5&_nc_ht=scontent-dfw5-2.xx&oh=86155c3e3a574b17264a5288058adafd&oe=5F941A72',
-  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120221566_10224237446952696_8305822572031120592_n.jpg?_nc_cat=105&_nc_sid=0debeb&_nc_ohc=zLrGBUPjdUoAX_dAMzB&_nc_ht=scontent-dfw5-1.xx&oh=b3e3e4c4e16f125b344c123bafe40f69&oe=5F953715'
+  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120221566_10224237446952696_8305822572031120592_n.jpg?_nc_cat=105&_nc_sid=0debeb&_nc_ohc=zLrGBUPjdUoAX_dAMzB&_nc_ht=scontent-dfw5-1.xx&oh=b3e3e4c4e16f125b344c123bafe40f69&oe=5F953715',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120260917_10224237446112675_9171576474610392466_n.jpg?_nc_cat=108&_nc_sid=0debeb&_nc_ohc=j8x6OScBTuIAX8gZjmn&_nc_ht=scontent-dfw5-2.xx&oh=7375b51e62be076ff3ebd1dc51e88d93&oe=5F994D7E',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120292229_10224237446512685_6897692524845047866_n.jpg?_nc_cat=100&_nc_sid=0debeb&_nc_ohc=LIjfErJGQSEAX8AnYc0&_nc_ht=scontent-dfw5-2.xx&oh=252f33c2d44f51775d4ff5ccc128c49a&oe=5F9C0372',
+  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120221566_10224237446952696_8305822572031120592_n.jpg?_nc_cat=105&_nc_sid=0debeb&_nc_ohc=PHvQ1dPITxwAX9BKaNQ&_nc_ht=scontent-dfw5-1.xx&oh=635c570354ab08032d3830e43cddc6fc&oe=5F992B95',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120260497_10224237447272704_1283966180043824362_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=PQ_NEIAdi9kAX9uFnD4&_nc_ht=scontent-dfw5-2.xx&oh=3c9bb74fa0447bda5f9d0bec34ee28bd&oe=5F99BFCF',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120199509_10224237447752716_4115313973868326029_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=Z42wnMcMRRoAX-adRuc&_nc_ht=scontent-dfw5-2.xx&oh=0c737cc6f07aeb5e2de40a5a11b04949&oe=5F9A0E02',
+  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120237326_10224237448032723_8628185843300472225_n.jpg?_nc_cat=101&_nc_sid=0debeb&_nc_ohc=x_mUVygODrsAX961w9p&_nc_ht=scontent-dfw5-1.xx&oh=7a3cc938ec4a1aeb16fafdcd070f8238&oe=5F9B6A33',
+  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120228107_10224242110949293_8410408492101870867_n.jpg?_nc_cat=103&_nc_sid=0debeb&_nc_ohc=6JdoGUdJbkoAX-nedQB&_nc_ht=scontent-dfw5-1.xx&oh=2a8f07f51e414c8fb7f6ea453c347c4e&oe=5F9C03A9',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120161243_10224242111189299_124448692554109691_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=9MhN0RyUs5EAX_5LIfc&_nc_ht=scontent-dfw5-2.xx&oh=a539513baef4223c84912d8d999f0891&oe=5F9A8E2B',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120254212_10224242116749438_8695276845653094576_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=PiR9whcc0qsAX9JSzZl&_nc_ht=scontent-dfw5-2.xx&oh=366a7156100143d9558ea3e94ba2f02f&oe=5F9AFB47',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120229521_10224242117029445_790290926545515117_n.jpg?_nc_cat=106&_nc_sid=0debeb&_nc_ohc=Kmlaiwz99fIAX--8-5Z&_nc_ht=scontent-dfw5-2.xx&oh=398531208acb7f117372928f9f388d5e&oe=5F9B5A5C',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120324904_10224242117269451_5244844093628807390_n.jpg?_nc_cat=106&_nc_sid=0debeb&_nc_ohc=G3yMXgnPTSYAX9rxVqr&_nc_ht=scontent-dfw5-2.xx&oh=78c009ba58bd91c781f5a7dabd7053b7&oe=5F9A64CC',
+  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120217573_10224242118749488_576469724548187856_n.jpg?_nc_cat=101&_nc_sid=0debeb&_nc_ohc=aaOL3ap7NgcAX83kHmG&_nc_ht=scontent-dfw5-1.xx&oh=752819ca4068e1292a3bdc6831a96126&oe=5F98EC23',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120352170_10224242118989494_4213111112551036779_n.jpg?_nc_cat=106&_nc_sid=0debeb&_nc_ohc=wvq9iRIKLp8AX-Y4ybO&_nc_ht=scontent-dfw5-2.xx&oh=4289673089fd0a9b69c14c4a1e2f8f40&oe=5F9BEE54',
+  'https://scontent-dfw5-1.xx.fbcdn.net/v/t1.0-9/120222184_10224242119229500_312163340488433830_n.jpg?_nc_cat=101&_nc_sid=0debeb&_nc_ohc=lLjV2lzSK90AX9A1HJ4&_nc_ht=scontent-dfw5-1.xx&oh=f59ca3d985d9208d5061cb4f12c67524&oe=5F9A2195',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120321834_10224242119549508_4816068977267705248_n.jpg?_nc_cat=104&_nc_sid=0debeb&_nc_ohc=ua7Nk89GL-EAX_ppaOj&_nc_ht=scontent-dfw5-2.xx&oh=bfa3eb928553c006bb6af41f0b47a025&oe=5F991419'
 ]
 
 class ShortCuts extends Component {
