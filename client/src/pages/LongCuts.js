46273ef82c7e8ee13eeafadc4ceea9fd33afc8d5
Steven Johnson <sjohnson21960@gmail.com>
2020-09-30 11:31:56 -0500

    update long cuts

--- client/src/pages/LongCuts.js
+++ client/src/pages/LongCuts.js
@@ -6,7 +6,10 @@ import { Carousel } from "react-bootstrap"
 import Nav from "../components/PortfolioNav"
 
 let longcuts = [
-  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120217381_10224237449232753_3984324590094121753_n.jpg?_nc_cat=100&_nc_sid=0debeb&_nc_ohc=RWD-l9-CP1oAX-Ks6QP&_nc_ht=scontent-dfw5-2.xx&oh=eb3ac125cd71bcbfdb42a1242236bcd1&oe=5F95D15B'
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120217381_10224237449232753_3984324590094121753_n.jpg?_nc_cat=100&_nc_sid=0debeb&_nc_ohc=RWD-l9-CP1oAX-Ks6QP&_nc_ht=scontent-dfw5-2.xx&oh=eb3ac125cd71bcbfdb42a1242236bcd1&oe=5F95D15B',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120217381_10224237449232753_3984324590094121753_n.jpg?_nc_cat=100&_nc_sid=0debeb&_nc_ohc=ZcB7M5_JZBAAX_yviiz&_nc_ht=scontent-dfw5-2.xx&oh=5bef8e76f020439860abbe24bc81ea24&oe=5F99C5DB',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120218734_10224242109909267_8658242274290037604_n.jpg?_nc_cat=108&_nc_sid=0debeb&_nc_ohc=DZwIcoYSKtsAX_gTlat&_nc_ht=scontent-dfw5-2.xx&oh=4dec5c95c1b8ade9cdc93ee87c1731fd&oe=5F985D75',
+  'https://scontent-dfw5-2.xx.fbcdn.net/v/t1.0-9/120192864_10224242110189274_5725863641494668283_n.jpg?_nc_cat=102&_nc_sid=0debeb&_nc_ohc=R_U2abhz-ukAX-wsQuW&_nc_ht=scontent-dfw5-2.xx&oh=6f08722c8b4acb1318ecf4d1a3c58358&oe=5F9BE74E'
 ]
 
 class ShortCuts extends Component {
